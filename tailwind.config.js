bb52808044b83040f155e0237cf4ca7e91640be4
3DHACKPLANT <147284474+kimminhyeok11@users.noreply.github.com>
2025-11-01 16:36:54 +0900

    tailwind.config.js 만들기

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Tailwind가 index.html 파일을 스캔하도록 설정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
